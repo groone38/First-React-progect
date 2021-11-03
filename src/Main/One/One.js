d61cf07ddce8b5eb3505ff25563d2c01f281f533
groone <groone38@yandex.ru>
2021-11-03 21:10:36 +0300

    Redesign

--- src/Main/One/One.js
+++ src/Main/One/One.js
@@ -5,17 +5,21 @@ function One() {
     return(
         <div className="wrap">
             <h2>Записаться</h2>
-            <div className="One">
-                <form action="" className="form">
-                    <div className="form-name">
+            <div className="one__conteiner">
+                <form action="http://localhost:3001/example" method="POST" className="conteiner__form">
+                    <div className="conteiner__form name">
                         <label htmlFor="name">Ваше имя</label>
-                        <input type="text" htmlFor="name" placeholder="Введите ваше имя"/>
+                        <input type="text" name="name" placeholder="Введите ваше имя"/>
                     </div>
-                    <div className="form-tel">
+                    <div className="conteiner__form name">
+                        <label htmlFor="email">Ваш email</label>
+                        <input type="text" name="email" placeholder="Введите ваш email"/>
+                    </div>
+                    <div className="conteiner__form tel">
                         <label htmlFor="tel">Укажите номер телефона</label>
                         <input type="number" name="tel" placeholder="8955855558" />
                     </div>
-                    <div className="form-area">
+                    <div className="conteiner__form area">
                         <label htmlFor="text">Дополнительная информация</label>
                         <textarea name="text" id="" cols="30" rows="10"></textarea>
                     </div>
